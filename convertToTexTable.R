20f06c037e9df7db2d278780d34605a59185e12f
jkmoerma <166617448+jkmoerma@users.noreply.github.com>
2024-08-22 14:07:35 +0200

    Add files via upload
    Last upload before a full run of the R-script as an ultimate test

--- convertToTexTable.R
+++ convertToTexTable.R
@@ -1,3 +1,26 @@
+
+#' Write a matrix or data frame to a .tex table, ready to include in your LaTeX report
+#' 
+#' @param tab matrix or data frame
+#' @param filename Name of the file the matrix or data frame must be written to. Using a .tex extension is advised, suitability for other file extensions was not checked
+#' @param caption Information and explanations about the table as a character string.
+#' @param reflabel Label of the .tex table for referencing as a character string.
+#' @param rows.named Logical, set to TRUE for matrices only. If TRUE, The rownames of the matrix are included in the .tex file
+#' @param minipage Logical, set to TRUE if the .tex table is included in a "minipage" environment in LaTeX
+#' @return 
+#' @examples 
+#' 
+#' getwd()
+#'
+#' tab <- matrix(1:9, ncol=3)
+#' colnames(tab) <- c("col1", "col2", "col3")
+#' rownames(tab) <- c("row1", "row2", "row3")
+#' 
+#' convertToTexTable(tab, "tab.tex")
+#' convertToTexTable(tab, "tabWithCaption.tex", caption="This is a test", reflabel="test_matrix")
+#' convertToTexTable(tab, "tabWithRowsNamed.tex", caption="This is a test", reflabel="test_matrix", rows.named=TRUE)
+#' convertToTexTable(tab, "tabMinipage.tex", minipage=TRUE)
+#' 
 convertToTexTable <- function(tab, filename, caption=NULL, reflabel=NULL, 
                               rows.named=FALSE, minipage=FALSE){
   sink(file=filename)
@@ -40,11 +63,4 @@ convertToTexTable <- function(tab, filename, caption=NULL, reflabel=NULL,
   }
   cat("\n")
   sink()
-}
-
-if (FALSE) {
-  # a check of functionality for this function
-  tab <- matrix(1:9, ncol=3)
-  convertToTexTable(tab, "tab1.tex")
-  convertToTexTable(tab, "tab2.tex", caption="This is a test", reflabel="test_matrix")
 }
